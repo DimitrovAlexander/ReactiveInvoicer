7a831f03ac0b610eeb166da3b949444a37505f58
Alexander Dimitrov <alexander_dimitrov@yahoo.com>
2024-12-02 12:54:28 +0200

    Added seed db

---
+++ ReactiveInvoicerBackEnd/ReactiveInvoicer/Models/DbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Common;
+
+namespace ReactiveInvoicer.Models
+{
+    public class DbInitializer
+    {
+        public static void Initialize(string conString)
+        {
+            // Ensure database is created
+            string sqlConnectionString = conString;
+
+            string script = File.ReadAllText(@"..\..\Database\ApplicationTrackerHR Insert.sql");
+
+            SqlConnection conn = new SqlConnection(sqlConnectionString);
+
+            Server server = new Server(new ServerConnection(conn));
+
+            server.ConnectionContext.ExecuteNonQuery(script);
+
+
+        }
+
+    }
