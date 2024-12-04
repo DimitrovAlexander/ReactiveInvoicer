b2f6991553f63399d41217bd7dc4a32501c20f1e
Dono <donnydonchev@gmail.com>
2024-12-04 12:29:04 +0200

    Update AppLayout.jsx

--- ReactiveInvoicerFrontEnd/ReactiveInvoicer/src/components/layout/AppLayout.jsx
+++ ReactiveInvoicerFrontEnd/ReactiveInvoicer/src/components/layout/AppLayout.jsx
@@ -1,4 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
+import "./AppLayout.css";
+
+const AppLayout = ({ onLoginClick }) => {
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+
+  return (
+    <div className="app-layout">
+      <Header onLoginClick={onLoginClick} />
+      <div className="layout-content">
+        <Sidebar
+          isCollapsed={isSidebarCollapsed}
+          onToggle={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
+        />
+        <main className="main-content">
+          <h1>Welcome to ReactiveInvoicer!</h1>
+        </main>
+      </div>
+      <Footer isSidebarCollapsed={isSidebarCollapsed} />
+    </div>
+  );
+};
+
+export default AppLayout;
+
+
+
+
+
+
+
+
+/*import React from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import "./AppLayout.css";
@@ -9,13 +44,13 @@ const AppLayout = ({ onLoginClick }) => {
       <Header onLoginClick={onLoginClick} />
       <div className="layout-content">
         <Sidebar />
-        <main className="main-content">
-          {/* Add the main page content here */}
-          <h1>Welcome to ReactiveInvoicer!</h1>
+        <main className="main-content">*/
+          /*{*//* Add the main page content here *//*}*/
+          /*<h1>Welcome to ReactiveInvoicer!</h1>
         </main>
       </div>
     </div>
   );
 };
 
-export default AppLayout;
+export default AppLayout;*/
