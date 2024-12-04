93f110cbcbd22576853078c933622d89100eb467
Dono <donnydonchev@gmail.com>
2024-12-04 12:29:23 +0200

    Update Footer.jsx

--- ReactiveInvoicerFrontEnd/ReactiveInvoicer/src/components/layout/Footer.jsx
+++ ReactiveInvoicerFrontEnd/ReactiveInvoicer/src/components/layout/Footer.jsx
@@ -1,23 +1,26 @@
-export default function Footer() {
-	return (
-		<footer className="footer bg-neutral text-neutral-content p-10">
-			<nav>
-				<h6 className="footer-title">Services</h6>
-				<a className="link link-hover">Marketing</a>
-				<a className="link link-hover">Advertisement</a>
-			</nav>
-			<nav>
-				<h6 className="footer-title">Company</h6>
-				<a className="link link-hover">About us</a>
-				<a className="link link-hover">Contact</a>
+import React from "react";
+import "./Footer.css";
 
-			</nav>
-			<nav>
-				<h6 className="footer-title">Legal</h6>
-				<a className="link link-hover">Terms of use</a>
-				<a className="link link-hover">Privacy policy</a>
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+      </nav>
+    </footer>
+  );
+};
 
-			</nav>
-		</footer>
-	);
-}
+export default Footer;
